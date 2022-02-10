8e1b844a192f5a169b0f0e6c2a2494c009466623
Mattias Norlander (EXT Exp) <mattias.norlander@gmail.com>
2022-02-10 08:37:19 +0100

    WIP

--- config.el
+++ config.el
@@ -3,9 +3,9 @@
 ;; Place your private configuration here
 (setq projectile-project-search-path '("~/projects/"))
 (setq projectile-enable-caching nil)
-(exec-path-from-shell-initialize)
-(exec-path-from-shell-copy-envs
- '("PATH"))
+;; (exec-path-from-shell-initialize)
+;; (exec-path-from-shell-copy-envs
+;; '("PATH"))
 ;; (add-to-list 'exec-path "~/.nvm/versions/node/v10.13.0/bin")
                                         ;(setq exec-path (prepend exec-path '("~/.nvm/versions/node/v10.13.0/bin")))
                                         ;(add-hook 'focus-out-hook 'projectile-save-project-buffers)
@@ -21,7 +21,7 @@
 (setenv "LANG" "sv_SE.UTF-8")
 
 ;; Refresh magit after save
-(add-hook 'after-save-hook 'magit-after-save-refresh-status t)
+;;(add-hook 'after-save-hook 'magit-after-save-refresh-status t)
 (setq magit-save-repository-buffers 'dontask)
 (setq magit-list-refs-sortby "creeatordate")
 
@@ -31,8 +31,62 @@
 (evil-snipe-override-mode 1)
 (setq evil-snipe-override-evil-repeat-keys t)
 (setq evil-snipe-repeat-keys t)
-;;(setq global-display-line-numbers-mode t)
+;;(setq global-display-line-nu
 (setq-default display-line-numbers-type 'relative)
 (map! :leader
       :desc "Format buffer" "c f" #'format-all-buffer)
-(define-key evil-normal-state-map (kbd "v") #'er/expand-region)
+;; (define-key evil-normal-state-map (kbd "v") #'er/expand-region)
+(setq-default fill-column 100)
+(setq-default scroll-margin 7)
+;; disable json-jsonlist checking for json files
+(add-hook 'es-result-mode-hook 'hs-minor-mode)
+(add-to-list 'auto-mode-alist '("\\.es$" . es-mode))
+(setq es-always-pretty-print t)
+
+(map! :nv "C-:" #'er/contract-region
+      :nv "C-;" #'er/expand-region)
+
+(defun js-to-json ()
+  (interactive)
+  (shell-command-on-region (region-beginning)
+                           (region-end)
+                           "node -e 'console.log(JSON.stringify(eval(\"(\" + require(\"fs\").readFileSync(0, \"utf-8\") + \")\"),null,2))'"
+                           (current-buffer)
+                           t))
+(map! :leader
+      :desc "convert js to json" "c j" #'js-to-json)
+(setq split-height-threshold nil)
+(setq split-width-threshold 160)
+;;; :ui doom-dashboard
+(setq fancy-splash-image (concat doom-private-dir "splash.png"))
+;; Hide the menu for as minimalistic a startup screen as possible.
+(remove-hook '+doom-dashboard-functions #'doom-dashboard-widget-shortmenu)
+
+;; Seems to make rjsx mode faster but not sure..
+(setq js-jsx-detect-syntax nil)
+
+;; otherwise emacs wont stick to its virtual window
+(menu-bar-mode t)
+(global-unset-key (kbd "C-e"))
+(use-package doom-themes
+  :custom-face ; for solarized dark
+                                        ; (font-lock-constant-face      ((t (:foreground "#2aa198"))))                       ; solarized cyanI
+                                        ;(highlight-numbers-number     ((t (:foreground "#2aa198"))))                       ; solarized cyan
+                                        ; (diff-hl-change               ((t (:background "#2aa198" :foreground "#2aa198")))) ; solarized cyan
+                                        ; (show-paren-match             ((t (:foreground "white" :background "#174652"))))
+  (highlight-symbol-face        ((t (:background "#073642")))) ; region (base0)
+  (highlight        ((t (:background "#073642")))) ; region (base0)
+  :config
+  (setq doom-themes-enable-bold nil)
+  (load-theme 'doom-one t))
+
+(setq format-all-debug 'true)
+(set-formatter! 'eslint  "eslint_d --fix-to-stdout --stdin")
+(set-formatter! 'prettier-js  "npx prettier --stdin-filepath foo.js")
+(setq-hook! 'rjsx-mode-hook +format-with (choose-formatter))
+(defun choose-formatter ()
+  (add-hook! 'before-save-hook #'+format/buffer)
+  (if (file-exists-p (concat projectile-project-root ".prettierrc"))
+      'prettier-js 'eslint))
+;; Rjxs mode keeps resizing this to show tooltips which is really annoing
+(setq max-mini-window-height 1)
--- init.el
+++ init.el
@@ -21,7 +21,7 @@
        :ui
        ;;deft              ; notational velocity for Emacs
        doom              ; what makes DOOM look the way it does
-                                        ;doom-dashboard    ; a nifty splash screen for Emacs
+       ;; doom-dashboard    ; a nifty splash screen for Emacs
        doom-quit         ; DOOM quit-message prompts when you quit Emacs
        fill-column       ; a `fill-column' indicator
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
@@ -30,7 +30,7 @@
        modeline          ; snazzy, Atom-inspired modeline, plus API
        nav-flash         ; blink the current line after jumping
        neotree           ; a project drawer, like NERDTree for vim
-       ophints           ; highlight the region an operation acts on
+       ;; ophints           ; highlight the region an operation acts on
        (popup            ; tame sudden yet inevitable temporary windows
         +all             ; catch all popups that start with an asterix
         +defaults)       ; default popup rules
@@ -47,7 +47,7 @@
        (evil +everywhere); come to the dark side, we have cookies
        file-templates    ; auto-snippets for empty files
        fold              ; (nigh) universal code folding
-       ;;(format +onsave)  ; automated prettiness
+       (format +onsave)  ; automated prettiness
        ;;lispy             ; vim for lisp, for people who dont like vim
        multiple-cursors  ; editing in many places at once
        ;;objed             ; text object editing for the innocent
@@ -60,7 +60,7 @@
        dired             ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
        vc                ; version-control and Emacs, sitting in a tree
-
+       undo              ; undo-fu
        :term
        ;;eshell            ; a consistent, cross-platform shell (WIP)
        ;;shell             ; a terminal REPL for Emacs
@@ -136,7 +136,7 @@
        ;;python            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
-       ;;rest              ; Emacs as a REST client
+       rest              ; Emacs as a REST client
        ;;ruby              ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
@@ -174,20 +174,3 @@
        ;; provides a Spacemacs-inspired keybinding scheme and a smartparens
        ;; config. Use it as a reference for your own modules.
        (default +bindings +smartparens))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(custom-enabled-themes (quote (doom-one)))
- '(custom-safe-themes
-   (quote
-    ("7f74a3b9a1f5e3d31358b48b8f8a1154aab2534fae82c9e918fb389fca776788" "423435c7b0e6c0942f16519fa9e17793da940184a50201a4d932eafe4c94c92d" "0fe9f7a04e7a00ad99ecacc875c8ccb4153204e29d3e57e9669691e6ed8340ce" "fe76f3d5094967034192f6a505085db8db6deb0e135749d9a54dc488d6d3ee2f" "5e0b63e0373472b2e1cf1ebcc27058a683166ab544ef701a6e7f2a9f33a23726" "cdb3e7a8864cede434b168c9a060bf853eeb5b3f9f758310d2a2e23be41a24ae" "2878517f049b28342d7a360fd3f4b227086c4be8f8409f32e0f234d129cee925" "0d087b2853473609d9efd2e9fbeac088e89f36718c4a4c89c568dd1b628eae41" "001c2ff8afde9c3e707a2eb3e810a0a36fb2b466e96377ac95968e7f8930a7c5" "e7666261f46e2f4f42fd1f9aa1875bdb81d17cc7a121533cad3e0d724f12faf2" "2d392972cbe692ee4ac61dc79907af65051450caf690a8c4d36eb40c1857ba7d" "2d1fe7c9007a5b76cea4395b0fc664d0c1cfd34bb4f1860300347cdad67fb2f9" "728eda145ad16686d4bbb8e50d540563573592013b10c3e2defc493f390f7d83" "332e009a832c4d18d92b3a9440671873187ca5b73c2a42fbd4fc67ecf0379b8c" "c8f959fb1ea32ddfc0f50db85fea2e7d86b72bb4d106803018be1c3566fd6c72" "2a3ffb7775b2fe3643b179f2046493891b0d1153e57ec74bbe69580b951699ca" "f951343d4bbe5a90dba0f058de8317ca58a6822faa65d8463b0e751a07ec887c" "071f5702a5445970105be9456a48423a87b8b9cfa4b1f76d15699b29123fb7d8" "fefab1b6d3366a959c78b4ed154018d48f4ec439ce652f4748ef22945ca7c2d5" "018c8326bced5102b4c1b84e1739ba3c7602019c645875459f5e6dfc6b9d9437" "f589e634c9ff738341823a5a58fc200341b440611aaa8e0189df85b44533692b" "a2286409934b11f2f3b7d89b1eaebb965fd63bc1e0be1c159c02e396afb893c8" "d6f04b6c269500d8a38f3fabadc1caa3c8fdf46e7e63ee15605af75a09d5441e" "66d53738cc824d0bc5b703276975581b8de2b903d6ce366cd62207b5dd6d3d13" "428754d8f3ed6449c1078ed5b4335f4949dc2ad54ed9de43c56ea9b803375c23" "70ed3a0f434c63206a23012d9cdfbe6c6d4bb4685ad64154f37f3c15c10f3b90" "f2b83b9388b1a57f6286153130ee704243870d40ae9ec931d0a1798a5a916e76" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
